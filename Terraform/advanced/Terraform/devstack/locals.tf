c2da389b3e1c4d95d38274e6947a397bbd0999e1
drajcan <rajcan.daniel@gmail.com>
2022-10-14 13:17:16 +0200

    Modify Terraform structure

--- Terraform/advanced/Terraform/devstack/locals.tf
+++
@@ -1,8 +0,0 @@
-# --- root/locals.tf ---
-
-locals {
-  deployment = {
-    tenant_name    = var.environment[terraform.workspace]["tenant_name"]
-    instance_count = var.environment[terraform.workspace]["instance_count"]
-  }
-}
---
+++ Terraform/advanced/locals.tf
@@ -0,0 +1,17 @@
+# --- root/locals.tf ---
+
+locals {
+  kis_os_region   = "RegionOne"
+  kis_os_auth_url = "http://158.193.138.33:5000/v3/"
+  kis_os_endpoint_overrides = {
+    compute = "http://158.193.138.33:8774/v2.1/"
+    image   = "http://158.193.138.33:9292/v2.0/"
+    network = "http://158.193.138.33:9696/v2.0/"
+  }
+  my_public_ip = "${data.http.my_public_ip.response_body}/32"
+
+  deployment = {
+    tenant_name    = var.environment[terraform.workspace]["tenant_name"]
+    instance_count = var.environment[terraform.workspace]["instance_count"]
+  } 
+}

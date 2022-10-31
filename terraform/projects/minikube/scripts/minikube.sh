60c3168cde209b988e64e6c3b9b48c58ab82e097
Daniel Rajcan <drajcan@ohpen.com>
2022-10-31 06:49:37 +0100

    Add variable for k8s version

--- terraform/projects/minikube/scripts/minikube.sh
+++ terraform/projects/minikube/scripts/minikube.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+K8S_VERSION="v1.23.13"
+
 ### conntrack: install
 apt install conntrack
 
 ### kubectl: install
-curl --silent -LO https://storage.googleapis.com/kubernetes-release/release/v1.23.13/bin/linux/amd64/kubectl
+curl --silent -LO https://storage.googleapis.com/kubernetes-release/release/$K8S_VERSION/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 
@@ -12,6 +14,6 @@ sudo mv ./kubectl /usr/local/bin/kubectl
 curl --silent -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
 
 ### minikube: start
-su - ubuntu -c "minikube start --kubernetes-version=v1.23.13 --nodes=3"
+su - ubuntu -c "minikube start --kubernetes-version=${K8S_VERSION} --nodes=3"
 su - ubuntu -c "minikube addons enable ingress"
 su - ubuntu -c "minikube status"
